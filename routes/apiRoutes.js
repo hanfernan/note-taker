5095123a645bba9074134721a20abe4b08ea27cf
Hannah Fernandes-Martin <hfm11295@gmail.com>
2021-04-16 19:47:06 -0400

    fixed bugs

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,7 @@
 //LOAD DATA
 const fs = require("fs");
 const db = require('../db/db.json');
+const path = require("path");
 
 //ROUTING
 
@@ -8,20 +9,25 @@ module.exports = (app) => {
     // * `GET /api/notes` should read the `db.json` file and return all saved notes as JSON.
     app.get('/api/notes', (req, res) => {
         console.log("route hit");
-        return res.sendFile(path.join(__dirname, "./db/db.json"));
+        return res.sendFile(path.join(__dirname, "../db/db.json"));
 
     });
 
+    //allows user to post new notes
     app.post('/api/notes', (req, res) => {
         //write file sync
         try {
             let notes = JSON.parse(fs.readFileSync("./db/db.json", 'utf-8'))
             notes.push(req.body)
+            res.end()
             fs.writeFileSync("./db/db.json", JSON.stringify(notes), "utf-8")
         } catch (error) {
             console.log(error)
         }
     });
+
+    
+
 
 }
 
